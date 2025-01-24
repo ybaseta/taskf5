9efbdbfdcda0dc1098747087ed01e0f8252e7570
ybatechtrainer <gestionyba@gmail.com>
2025-01-24 19:22:39 +0100

    initial commit

---
+++ src/main/java/com/example/taskf5/Taskf5Application.java
@@ -0,0 +1,13 @@
+package com.example.taskf5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taskf5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taskf5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskf5/Taskf5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskf5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taskf5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
